b7816c1f988be554ddf69f82606f904a28ad8bdb
sota <sota1210nittc@gmail.com>
2021-03-22 11:26:10 +0900

    カメラの修正

--- Assets/BlockPuzzle/PutBlock.cs
+++ Assets/BlockPuzzle/PutBlock.cs
@@ -7,8 +7,15 @@ public class PutBlock : MonoBehaviour
     public const int height = 6;
     public const int width = 6;
 
+    private GameObject[] blocks;
+
+    SelectController select;
+    float time;
+
     void Start()
     {
+        //BlockPuzzleが見えるような位置に回転
+        GameObject.Find("Main Camera").transform.Rotate(-90, 0, 0);
         Init();
     }
 
@@ -18,16 +25,34 @@ public class PutBlock : MonoBehaviour
 
     void Init()
     {
+        select = GameObject.Find("PuzzleSelect").GetComponent<SelectController>();
+
+        blocks = new GameObject[9];
         //ボードを設置
-        GameObject board = (GameObject)Resources.Load("Images/BlockPuzzle/board");
-        Instantiate(board);
+        blocks[0] = Instantiate((GameObject)Resources.Load("Images/BlockPuzzle/board"));
 
         //パズルに使用するブロックをすべて生成
         for (int i = 1; i <= 8; i++)
         {
-            GameObject prefab = (GameObject)Resources.Load("Images/BlockPuzzle/group" + i.ToString());
-            Instantiate(prefab);
+            blocks[i] = Instantiate((GameObject)Resources.Load("Images/BlockPuzzle/group" + i.ToString()));
         }
+    }
+
+    void Finish()
+    {
+        //カメラの位置を元に戻す
+        GameObject.Find("Main Camera").transform.Rotate(90, 0, 0);
+
+        foreach (GameObject block in blocks) Destroy(block);
+        Destroy(this.gameObject);
+
+        string sub = "";
+        float t = Time.time - time;
+        int min = (int)(t / 60);
+        int sec = (int)(t % 60);
+        if (sec < 10) sub = "0";
+        string result = min.ToString() + ":" + sub + sec.ToString();
+        select.EndPuzzle(result);
     }
 
     public bool checkIsAllCorrect()
@@ -43,6 +68,8 @@ public class PutBlock : MonoBehaviour
             }
         }
         Debug.Log("All Correct!!");
+        //Finish();
+        Invoke(nameof(Finish), 1.5f);
         return true;
     }
 }
